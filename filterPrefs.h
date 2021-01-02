93c024a5ce88a7492c7359e6bd03cf6c63eaa74e
r-plus <anasthasia.r@gmail.com>
2021-01-03 00:50:38 +0900

    Update filterPrefs to avoid using PLFilterKey

--- filterPrefs.h
+++ filterPrefs.h
@@ -1,22 +1,45 @@
 #import <Preferences/Preferences.h>
 #import <libprefs/prefs.h>
+#define kPLFilterKey @"pl_filter"
 
-static inline id FilteredSpecifiers(id _specifiers)
-{
-    NSMutableArray *removals = [NSMutableArray array];
+#if !__has_feature(objc_arc)
+    static inline id FilteredSpecifiers(id _specifiers)
+    {
+        NSMutableArray *removals = [NSMutableArray array];
 
-    for (id spec in _specifiers) {
-        if (![PSSpecifier environmentPassesPreferenceLoaderFilter:[spec propertyForKey:PLFilterKey]])
-            [removals addObject:spec];
+        for (id spec in _specifiers) {
+            if (![PSSpecifier environmentPassesPreferenceLoaderFilter:[spec propertyForKey:kPLFilterKey]])
+                [removals addObject:spec];
+        }
+
+        // remove
+        if (removals.count > 0) {
+          NSMutableArray *newSpecifiers = [_specifiers mutableCopy];
+          [_specifiers release];
+          [newSpecifiers removeObjectsInArray:removals];
+          _specifiers = newSpecifiers;
+        }
+
+        return _specifiers;
     }
+#else
+    static inline id FilteredSpecifiers(id _specifiers)
+    {
+        NSMutableArray *removals = [NSMutableArray array];
 
-    // remove
-    if (removals.count > 0) {
-      NSMutableArray *newSpecifiers = [_specifiers mutableCopy];
-      [_specifiers release];
-      [newSpecifiers removeObjectsInArray:removals];
-      _specifiers = newSpecifiers;
+        for (id spec in _specifiers) {
+            NSDictionary *d = [spec propertyForKey:kPLFilterKey];
+            if (![PSSpecifier environmentPassesPreferenceLoaderFilter:d])
+                [removals addObject:spec];
+        }
+
+        // remove
+        if (removals.count > 0) {
+          NSMutableArray *newSpecifiers = [_specifiers mutableCopy];
+          [newSpecifiers removeObjectsInArray:removals];
+          _specifiers = newSpecifiers;
+        }
+
+        return _specifiers;
     }
-
-    return _specifiers;
-}
+#endif
